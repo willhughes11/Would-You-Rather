c25219f7acc3f6ceb24c20abff2de7324ab639e4
willhughes11 <willhughes2110@gmail.com>
2021-08-02 23:02:33 -0400

    update

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -6,30 +6,39 @@ import { privateRouteMapStateToProps } from './MapStateToProps';
 class PrivateRoute extends Component {
     render() {
         const { component: Component, authenticated, path, ...rest } = this.props
-
-        if (authenticated) {
-            return (
-                <Route path={path} {...rest} render={props => (
-                    <Component {...rest} {...props} />
-                )} />
-            )
-        } 
-        else if (localStorage.getItem('authUser')) {
-            localStorage.removeItem('authUser');
-            return (
-                <Route path={path} {...rest} render={props => (
-                    <Redirect push to={{ pathname: '/not-found', state: { from: props.location } }} />
-                )} />
-            )
-        } 
-        else {
-            localStorage.removeItem('authUser');
-            return (
-                <Route path={path} {...rest} render={props => (
-                    <Redirect push to={{ pathname: '/login', state: { from: props.location } }} />
-                )} />
-            )
-        }
+        console.log(authenticated)
+        return(
+            <Route exact path={path} render={() => (
+                authenticated ? (
+                    <Component {...rest}/>
+                ) : (
+                    <Redirect to='/login' />
+                )
+            )}/>
+        )
+        // if (authenticated) {
+        //     return (
+        //         <Route path={path} {...rest} render={props => (
+        //             <Component {...rest} {...props} />
+        //         )} />
+        //     )
+        // } 
+        // else if (localStorage.getItem('authUser')) {
+        //     localStorage.removeItem('authUser');
+        //     return (
+        //         <Route path={path} {...rest} render={props => (
+        //             <Redirect push to={{ pathname: '/not-found', state: { from: props.location } }} />
+        //         )} />
+        //     )
+        // } 
+        // else {
+        //     localStorage.removeItem('authUser');
+        //     return (
+        //         <Route path={path} {...rest} render={props => (
+        //             <Redirect push to={{ pathname: '/login', state: { from: props.location } }} />
+        //         )} />
+        //     )
+        // }
     }
 }
 
